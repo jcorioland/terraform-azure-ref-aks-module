4aa06a419ed8d5e877a1635b45830d662900e215
Ulrich Ntella <ulrich.creatella@gmail.com>
2021-09-19 14:35:07 +0200

    updated readme and removed location variable if it's not mandatory

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,7 +1,6 @@
 module "tf-ref-aks-module" {
   source             = "../../"
   environment        = "${var.environment}"
-  location           = "${var.location}"
   kubernetes_version = "${var.kubernetes_version}"
   ssh_public_key     = "${file(var.ssh_public_key_file)}"
 }
--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -1,7 +1,6 @@
 module "tf-ref-aks-module" {
   source                          = "../../"
   environment                     = var.environment
-  location                        = var.location
   kubernetes_version              = var.kubernetes_version
   service_principal_client_id     = var.service_principal_client_id
   service_principal_client_secret = var.service_principal_client_secret
